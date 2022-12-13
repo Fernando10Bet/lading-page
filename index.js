578fabd5b6c631ca3af3653accdb7ee79002c871
Fernando Peralta <fernando.p@10betlatam.com>
2022-12-13 11:59:01 -0600

    se moficia el cuando aparece el boton al final de la pantalla

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let alto_header = document.querySelector('header').clientHeight;
+let alto_pagos = document.querySelector('#casino-imgs').clientHeight;
+let alto_total = alto_header + alto_pagos;
 let alto_pantalla = window.screen.height;
 let boton = document.querySelector('.btn_join');
-let diferencia = Math.abs(alto_header - alto_pantalla);
+let diferencia = Math.abs(alto_total - alto_pantalla);
 window.onscroll = function(){
     if(window.scrollY > diferencia){
         boton.style.display = 'flex';
