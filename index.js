25f0097ee1e57ca6a47de1b7dd1eed5a8d8f9b03
Fernando Peralta <fernando.p@10betlatam.com>
2022-12-14 16:34:32 -0600

    Se modifca el orden de los elementos en header de version movil, se modifca la disposicion de los steps , se cambia la tercer imagen de las imagenes del fondo de las cards

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let alto_header = document.querySelector('header').clientHeight;
 let alto_pagos = document.querySelector('#casino-imgs').clientHeight;
-let alto_total = alto_header + alto_pagos;
+let alto_pasos = document.querySelector('.header-steps').clientHeight;
 let alto_pantalla = window.screen.height;
 let boton = document.querySelector('.btn_join');
+let alto_total = alto_header + alto_pagos + alto_pasos + boton.clientHeight;
 let diferencia = Math.abs(alto_total - alto_pantalla);
 window.onscroll = function(){
     if(window.scrollY > diferencia){
