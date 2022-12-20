0a0d1d5d6d08bc04cebcd0601b9cf9b5b4ee6e2a
Fernando Peralta <fernando.p@10betlatam.com>
2022-12-20 12:33:45 -0600

    se agregan los steps en el 100vh

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ let alto_pagos = document.querySelector('#casino-imgs').clientHeight;
 let alto_pasos = document.querySelector('.header-steps').clientHeight;
 let alto_pantalla = window.screen.height;
 let boton = document.querySelector('.btn_join');
-let alto_total = alto_header + alto_pagos + alto_pasos + boton.clientHeight;
+
+//100% de la pantalla incluya los steps;
+
+let alto_contenido = document.querySelector('.header-bg');
+
+if (window.screen.width > 1050) {
+    alto_contenido.style.height = `calc(100vh - ${alto_pasos}px)`
+    window.onresize = function () {
+        alto_contenido.style.height = `calc(100vh - ${alto_pasos}px)`
+    }
+}
+
+//Toogle del boton inferior de la pantalla
+let alto_total = alto_pagos + alto_pasos + boton.clientHeight + alto_contenido.clientHeight;
 let diferencia = Math.abs(alto_total - alto_pantalla);
-window.onscroll = function(){
-    if(window.scrollY > diferencia){
+window.onscroll = function () {
+    if (window.scrollY > diferencia) {
         boton.style.display = 'flex';
-    }else{
+    } else {
         boton.style.display = 'none';
     }
 }
