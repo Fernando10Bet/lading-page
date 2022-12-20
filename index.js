31f0861c0a8c4dfe1f176ecf74fa6b17352c9b73
Fernando Peralta <fernando.p@10betlatam.com>
2022-12-20 15:12:53 -0600

    mejoras

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let boton = document.querySelector('.btn_join');
 
 //100% de la pantalla incluya los steps;
 
-let alto_contenido = document.querySelector('.header-bg');
+/* let alto_contenido = document.querySelector('.header-bg');
 
 if (window.screen.width >= 1100) {
     alto_contenido.style.height = `calc(100vh - ${alto_pasos}px)`
@@ -17,10 +17,10 @@ window.onresize = function () {
     }else{
         alto_contenido.style.height = `auto`
     }
-}
+} */
 
 //Toogle del boton inferior de la pantalla
-let alto_total = alto_pagos + alto_pasos + boton.clientHeight + alto_contenido.clientHeight;
+let alto_total = alto_pagos + alto_pasos + boton.clientHeight + alto_header;
 let diferencia = Math.abs(alto_total - alto_pantalla);
 window.onscroll = function () {
     if (window.scrollY > diferencia) {
